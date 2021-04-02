9ee1850ef84644ecf74e6a796ce03d3204b15470
titan991 <titan.991@mail.ru>
2021-04-02 23:04:34 +0400

    init project

---
+++ src/main/java/com/study/StudyHelperApplication.java
@@ -0,0 +1,13 @@
+package com.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyHelperApplication.class, args);
+    }
+
+}
